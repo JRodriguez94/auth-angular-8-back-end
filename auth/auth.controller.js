976f3fdf4f890471d36889451461388c6961f824
Usuario <usuario@localhost.localdomain>
2019-08-15 16:18:34 -0500

    change to deployd mode

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -34,7 +34,7 @@ exports.loginUser = (req, res, next) => {
     email: req.body.email,
     password: req.body.password
   }
-  console.log("Request.body", req);
+  console.log("Request.body", req.body);
   User.findOne({ email: userData.email }, (err, user) => {
     if (err) return res.status(500).send('Server error!');
 
--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,6 @@
 
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: 'mongodb://localhost:27017/loginMean'
+    DB: 'mongodb://localhost:27017/loginMean',
+    BDR: 'mongodb+srv://josue:Ako12345@cluster0-gvt0v.mongodb.net/test?retryWrites=true&w=majority'
   }
