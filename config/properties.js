49cfdf1be949bf6e12139416322fc262fc9c4784
Usuario <usuario@localhost.localdomain>
2019-08-15 17:25:41 -0500

    some sht

--- config/properties.js
+++ config/properties.js
@@ -2,5 +2,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     DB: 'mongodb://localhost:27017/loginMean',
-    DBR: 'mongodb+srv://josue:Ako12345@cluster0-gvt0v.mongodb.net/test?retryWrites=true&w=majority'
+    DBR: 'mongodb+srv://josue:Ako12345@cluster0-gvt0v.mongodb.net/test?retryWrites=true&w=majority/loginMean'
   }
